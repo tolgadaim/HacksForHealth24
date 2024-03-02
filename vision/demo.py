492cb3827f82b1a39da90b0a6d3630210a0628b2
Sean Pandaleon <112599537+Sean-Panda@users.noreply.github.com>
2024-03-02 13:37:13 -0500

    label detection

--- vision/demo.py
+++ vision/demo.py
@@ -3,3 +3,16 @@ import os
 import pandas as pd
 from google.cloud import vision
 
+#Instantiates a client
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/Sean/Desktop/CSE/HacksForHealth24/vision/client_file.json'
+client = vision.ImageAnnotatorClient()
+
+#local image (url option is available too)
+image_path = 'C:/Users/Sean/Desktop/CSE/HacksForHealth24/vision/example_images/sky.webp'
+with io.open(image_path, 'rb') as image_file:
+    content = image_file.read()
+image = vision.Image(content=content)
+
+response = client.label_detection(image = image)
+for label in response.label_annotations: 
+    print(response)
