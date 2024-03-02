57304a6618680c8c05217fac4c4a0e98e3b5923a
Sean Pandaleon <112599537+Sean-Panda@users.noreply.github.com>
2024-03-02 14:53:31 -0500

    txt detect

--- vision/text_detection.py
+++ vision/text_detection.py
@@ -5,25 +5,26 @@ from google.cloud import vision
 from google_vision_ai import VisionAI
 from google_vision_ai import prepare_image_local
 
+
 #Instantiates a client
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/Sean/Desktop/CSE/HacksForHealth24/vision/client_file.json'
 client = vision.ImageAnnotatorClient()
 
-# #local image (url option is available too)
-# image_path = 'C:/Users/Sean/Desktop/CSE/HacksForHealth24/vision/example_images/sky.webp'
-# with io.open(image_path, 'rb') as image_file:
-#     content = image_file.read()
-# image = vision.Image(content=content)
-
-# response = client.label_detection(image = image)
-# for label in response.label_annotations: 
-#     print(label.description)
-#     print(label.score)
-
-image_path = 'C:/Users/Sean/Desktop/CSE/HacksForHealth24/vision/example_images/sky.webp'
+image_path = 'C:/Users/Sean/Desktop/CSE/HacksForHealth24/vision/example_images/zicam.JPG'
 image = prepare_image_local(image_path)
 va = VisionAI(client, image)
-label_detections = va.label_detection()
+import sys
+export = ""
+texts = va.text_detection()
+for indx, text in enumerate(texts):
+    print(text.description)
+    if "TYLENOL" in text.description:
+        export = "TYLENOL"
+    if "ZICAM" in text.description:
+        export = "ZICAM"
+    #draw_boundary(image_path, text.bounding_poly, text.description)
+    if indx > 3:
+        break
 
-df = pd.DataFrame(label_detections)
-print(df)
+print(export)
+sys.exit()
